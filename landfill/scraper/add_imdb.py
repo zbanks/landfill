a346ed6c5f755bf341002affeb6c3cdaac7d9a5b
Eric Van Albert <eric@ericslaptop.(none)>
2013-12-31 01:24:47 -0500

    simplifying scrapers

---
+++ landfill/scraper/add_imdb.py
@@ -0,0 +1,30 @@
+from add_movie import add_movie
+import requests
+import re
+import json
+
+def get_omdb_info(imdb):
+	m=re.match(r'(?:http://)?(?:www\.)?imdb\.com/title/(tt\d+)/',imdb)
+	if m is None:
+		raise Exception('Not a valid IMDB link')
+	imdb_id=m.groups()[0]
+	omdb=requests.get('http://www.omdbapi.com/?i={0}'.format(imdb_id)).text
+	j=json.loads(omdb)
+	if j['Response']!='True':
+		raise Exception('OMDB Query failed')
+	info={'meta':omdb}
+	if 'Title' in j: info['title']=j['Title']
+	if 'Year' in j: info['year']=j['Year']
+	if 'Poster' in j:
+		p=j['Poster']
+		m=re.match(r'(.+@@).+\.jpg',j['Poster'])
+		if m is not None:
+			p='{0}.jpg'.format(m.groups()[0])
+		info['thumb']=p
+	return info
+
+def add_movie_from_imdb(local_file,imdb):
+	add_movie(local_file,get_omdb_info(imdb))
+
+if __name__=='__main__':
+	add_movie_from_imdb('bbtest.mkv','http://www.imdb.com/title/tt0903747/')
---
+++ landfill/scraper/add_movie.py
@@ -0,0 +1,41 @@
+from scraper import *
+import settings
+import tempfile
+import sqlite3
+import subprocess
+import os
+import shutil
+import threading
+
+def title2file(title):
+		acceptable_chars='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789()'
+		return (''.join([l for l in title if l in acceptable_chars]))+'.mp4'
+
+
+def add_movie(local_file,info):
+	info['filename']=title2file(info['title'])
+	conn=sqlite3.connect(os.path.join(BASEPATH,settings.DB))
+	tf=tempfile.NamedTemporaryFile(delete=False)
+	try:
+		ffmpeg_proc=subprocess.Popen(settings.FFMPEG_QUERY(local_file),stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+		ffmpeg_out=ffmpeg_proc.communicate()
+		acopy=("Audio: {0}".format(settings.FFMPEG_ACODEC) in ffmpeg_out[1])
+		vcopy=("Video: {0}".format(settings.FFMPEG_VCODEC) in ffmpeg_out[1])
+		result=subprocess.call(settings.FFMPEG_ENCODE(local_file,tf.name,acopy,vcopy))
+		if result != 0:
+			raise Exception('FFMPEG Failed')
+
+		c=conn.cursor()
+		prune=[(k,v) for (k,v) in info.items() if k in ['filename','title','year','meta','thumb']]
+		keys=[p[0] for p in prune]
+		values=[p[1] for p in prune]
+		
+		c.execute('INSERT INTO movies ({0}) values ({1})'.format(','.join(keys),','.join(['?']*len(values))),[str(v) for v in values])
+		target=os.path.join(BASEPATH,settings.CONTENT_DIR,'movies',str(c.lastrowid),info['filename'])
+		os.makedirs(os.path.dirname(target))
+		shutil.copy(tf.name,target)
+		conn.commit()
+	finally:
+		os.unlink(tf.name)
+		conn.close()
+
